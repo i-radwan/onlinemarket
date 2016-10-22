55b81931fcee4de9bde0073eb68ef1b6e023e19b
Ibrahim Ali Radwan <i.radwan1996@gmail.com>
2016-10-22 17:23:56 +0200

    Add main content for displaying products

--- index.php
+++ index.php
@@ -4,9 +4,10 @@
 <head>
 	<meta charset="UTF-8">
 	<title>OMarket</title>
-	<link href="css/style.css" rel="stylesheet">
 	<link href="css/font-awesome/css/font-awesome.min.css" rel="stylesheet">
 	<link href="https://fonts.googleapis.com/css?family=Comfortaa|News+Cycle|Raleway" rel="stylesheet">
+	<link href="css/style.css" rel="stylesheet">
+
 	<script src="js/jquery.min.js"></script>
 </head>
 
@@ -37,28 +38,49 @@
 		<div id="content" class="content">
 			<section class="left-menu" id="left-menu">
 				<ul class="left-menu--list">
-					<li class="left-menu--listitem"><a href="#">&#187; Computers, IT &amp; Networking</a></li>
-					<li class="left-menu--listitem"><a href="#">&#187; Mobile Phones, Tablets &amp; Accessories</a></li>
-					<li class="left-menu--listitem"><a href="#">&#187; Car Electronics &amp; Accessories</a></li>
-					<li class="left-menu--listitem"><a href="#">&#187; Books</a></li>
-					<li class="left-menu--listitem"><a href="#">&#187; Gaming</a></li>
-					<li class="left-menu--listitem"><a href="#">&#187; Electronic</a>s</li>
-					<li class="left-menu--listitem"><a href="#">&#187; Sports &amp; Fitness</a></li>
-					<li class="left-menu--listitem"><a href="#">&#187; Perfumes &amp; Fragrances</a></li>
-					<li class="left-menu--listitem"><a href="#">&#187; Health &amp; Personal Care</a></li>
-					<li class="left-menu--listitem"><a href="#">&#187; Furniture</a></li>
-					<li class="left-menu--listitem"><a href="#">&#187; Apparel, Shoes &amp; Accessories</a></li>
-					<li class="left-menu--listitem"><a href="#">&#187; Appliances</a></li>
-					<li class="left-menu--listitem"><a href="#">&#187; Art, Crafts &amp; Collectables</a></li>
-					<li class="left-menu--listitem"><a href="#">&#187; Baby</a></li>
-					<li class="left-menu--listitem"><a href="#">&#187; Bed &amp; Bath</a></li>
-					<li class="left-menu--listitem"><a href="#">&#187; Kitchen &amp; Home Supplies</a></li>
-					<li class="left-menu--listitem"><a href="#">&#187; Toys</a></li>
+					<li class="left-menu--listitem"><a href="#">Computers, IT &amp; Networking</a></li>
+					<li class="left-menu--listitem"><a href="#">Mobile Phones, Tablets &amp; Accessories</a></li>
+					<li class="left-menu--listitem"><a href="#">Car Electronics &amp; Accessories</a></li>
+					<li class="left-menu--listitem"><a href="#">Books</a></li>
+					<li class="left-menu--listitem"><a href="#">Gaming</a></li>
+					<li class="left-menu--listitem"><a href="#">Electronic</a>s</li>
+					<li class="left-menu--listitem"><a href="#">Sports &amp; Fitness</a></li>
+					<li class="left-menu--listitem"><a href="#">Perfumes &amp; Fragrances</a></li>
+					<li class="left-menu--listitem"><a href="#">Health &amp; Personal Care</a></li>
+					<li class="left-menu--listitem"><a href="#">Furniture</a></li>
+					<li class="left-menu--listitem"><a href="#">Apparel, Shoes &amp; Accessories</a></li>
+					<li class="left-menu--listitem"><a href="#">Appliances</a></li>
+					<li class="left-menu--listitem"><a href="#">Art, Crafts &amp; Collectables</a></li>
+					<li class="left-menu--listitem"><a href="#">Baby</a></li>
+					<li class="left-menu--listitem"><a href="#">Bed &amp; Bath</a></li>
+					<li class="left-menu--listitem"><a href="#">Kitchen &amp; Home Supplies</a></li>
+					<li class="left-menu--listitem"><a href="#">Toys</a></li>
 				</ul>
 			</section>
 			<section class="main-content" id="main-content">
 				<div class="category-items-short">
 					<a href="#" class="category-items-short--title">Computers, IT &amp; Networking</a>
+					<div class="categories-items-container">
+						<div class="categories-single-item">
+							<h2 class="categories-single-item--title">IPhone 6S</h2>
+							<img class="categories-single-item--img" src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/IPhone_7_Jet_Black.svg/2000px-IPhone_7_Jet_Black.svg.png">
+							<div class="categories-single-item--visible-details">
+								<p class="categories-single-item--visible-details--spec"><strong class="categories-single-item--visible-details--spec--key">Price:</strong><span class="categories-single-item--visible-details--spec--val">500$</span></p>
+								<p class="categories-single-item--visible-details--spec"><strong class="categories-single-item--visible-details--spec--key">Rate:</strong><span class="categories-single-item--visible-details--spec--val">4.5/5</span></p>
+							</div>
+							<div class="categories-single-item--load-more">
+								<i class="fa fa-arrow-down categories-single-item--load-more--arrow" aria-hidden="true"></i> <span class="categories-single-item--load-more--text">more</span>
+								<i class="fa fa-arrow-down categories-single-item--load-more--arrow" aria-hidden="true"></i>
+
+							</div>
+							<div class="categories-single-item--invisible-details"></div>
+							<div class="categories-single-item--buy">
+								
+							</div>
+						</div>
+						<div class="categories-single-item">B</div>
+						<div class="categories-single-item">C</div>
+					</div>
 				</div>
 				<div class="category-items-short">
 					<a href="#" class="category-items-short--title">Mobile Phones, Tablets &amp; Accessories</a>
@@ -71,7 +93,7 @@
 				</div>
 			</section>
 		</div>
-		
+
 	</div>
 	<script src="js/main.js"></script>
 </body>
